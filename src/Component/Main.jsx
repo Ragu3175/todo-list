5be5b22b43194780b5a38d7798ec0fea86ccd3a2
Ragu3175 <ragu317317@gmail.com>
2026-03-20 11:36:59 +0530

    Refactor Main component with state and effects

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -1,4 +1,89 @@
-const handleChange = (e) => {
-    console.log('handle change');
+import { useState, useEffect } from "react";
+import { useRef } from "react";
+import "./Main.css";
+import Search from "./Search";
+import Itemslist from "./Itemslist";
+import Addbutton from "./Addbutton";
+function Main() {
+  const API_URL = "http://localhost:4000/items";
+  const [items, setItems] = useState([]);
+  const [name, setName] = useState("");
+  const [search, setSearch] = useState("");
+  const[fetcherror,setfetchError] = useState(null);
+
+  useEffect(() => {
+    // JSON.parse(localStorage.getItem("todo_list"));
+    const fetchItems = async() => {
+      try{
+        const response = await fetch(API_URL);
+        if(!response.ok) throw console.error("data not received");
+        const newItems = await response.json();
+        console.log(newItems)
+        setItems(newItems);
+        setfetchError(null)
+      }catch(err){
+          setfetchError(err.message)
+      }
+    }
+    (async()=> await fetchItems())()
+  }, []);
+  const handleChange = (e) => {
     setName(e.target.value);
   };
+  const inputRef = useRef();
+  const handleClick = () => {
+    if (name === "") {
+      alert("please enter list");
+      return;
+    }
+    const updateItems = { id: Date.now(), checked: false, name: name };
+    const newItems = [...items, updateItems];
+    setItems(newItems);
+    setName("");   
+  };
+  const handlenewChange = (id) => {
+    const newItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(newItems);
+  };
+  const handleDelete = (id) => {
+    const newItems = items.filter((item) => item.id !== id);
+    setItems(newItems);
+
+  };
+  const handleSearch = (e) => {
+    setSearch(e);
+  };
+  return (
+    <div className="Main1">
+      <header id="headpart">
+        <h1>TO DO LIST</h1>
+      </header>
+      <div className="main11">
+        <Search Search={search} handleSearch={handleSearch} />
+        <Addbutton
+          name={name}
+          handleChange={handleChange}
+          handleClick={handleClick}
+          inputRef={inputRef}
+        />
+        <main>
+          {fetcherror && <p>Error : {fetcherror}`</p>}
+        <Itemslist
+          items={items}
+          Search={search}
+          handlenewChange={handlenewChange}
+          handleDelete={handleDelete}
+        />
+        </main>
+      </div>
+      <div>
+        <footer id="footer">
+          <h2>TOTAL ITEMS : {items.length}</h2>
+        </footer>
+      </div>
+    </div>
+  );
+}
+export default Main;
