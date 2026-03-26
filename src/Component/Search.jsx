5f14522943202d84c092289a7c9aeb61b168b380
Ragu <ragu317317@gmail.com>
2026-03-26 10:06:40 +0530

    Online css search.js

--- src/Component/Search.jsx
+++ src/Component/Search.jsx
@@ -1,6 +1,7 @@
 function Search( { handleSearch, Search } ) {
   return (
       <div>
+          <h2 style={{ textAlign: 'center', color: '#333' }}>Search</h2>
            <div id="searchcont">
                   <input type="text" value={Search} onChange={( e ) => handleSearch( e.target.value )} placeholder="Search" />
             </div>
